06fb25c3ff2acda8d8948d8ae034b9e14c48af21
laibaa09 <115113457+laibaa09@users.noreply.github.com>
2024-04-21 11:32:15 -0700

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -8,6 +8,61 @@ package Task2;
  *
  * @author faiqa
  */
-public class Task2 {
-    
+public class Task {
+    interface IPerson {
+    String getName();
+}
+
+// Define Villager class implementing IPerson interface
+class Villager implements IPerson {
+    public String getName() {
+        return "Village Person";
+    }
+}
+
+// Define CityPerson class implementing IPerson interface
+class CityPerson implements IPerson {
+    public String getName() {
+        return "City Person";
+    }
+}
+
+// Define PersonType enum
+enum PersonType {
+    Rural, Urban
+}
+
+// Define PersonFactory class
+class PersonFactory {
+    public IPerson getPerson(PersonType type) {
+        switch (type) {
+            case Rural:
+                return new Villager();
+            case Urban:
+                return new CityPerson();
+            default:
+                throw new UnsupportedOperationException();
+        }
+    }
+}
+
+// Main class
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of PersonFactory
+        PersonFactory factory = new PersonFactory();
+
+        // Get and print Rural person
+        IPerson ruralPerson = factory.getPerson(PersonType.Rural);
+        System.out.println(ruralPerson.getName());
+
+        // Get and print Urban person
+        IPerson urbanPerson = factory.getPerson(PersonType.Urban);
+        System.out.println(urbanPerson.getName());
+    }
+}
+
+}
+
+
 }
