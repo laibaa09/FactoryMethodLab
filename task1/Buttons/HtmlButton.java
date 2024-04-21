950cd9303f3dc1a38a32258032b1c25284ae7078
laibaa09 <115113457+laibaa09@users.noreply.github.com>
2024-04-21 11:35:24 -0700

    Update HtmlButton.java

--- task1/Buttons/HtmlButton.java
+++ task1/Buttons/HtmlButton.java
@@ -6,8 +6,15 @@ package task1.Buttons;
 
 /**
  *
- * @author faiqa
+ * @author laiba
  */
 public class HtmlButton {
-    
+    public void render() {
+        System.out.println("<button>Test Button</button>");
+        onClick();
+    }
+
+    public void onClick() {
+        System.out.println("Click! Button says - 'Hello World!'");
+    }
 }
